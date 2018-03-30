b46f63abeedcbfc107e6bfebf608c82396ddab93
ADFGE\afernandezl <ing.a.landero@gmail.com>
2018-03-30 13:48:08 -0600

    Modificaion Tabs Abogado

--- resources/views/forms/abogado.blade.php
+++ resources/views/forms/abogado.blade.php
@@ -9,50 +9,49 @@
     {!! Form::open(['route' => 'store.abogado', 'method' => 'POST'])  !!}
 	{{ csrf_field() }}
 	<div class="card-header">@include('forms.buttons') </div>
-	<div class="card">
-		<div class="card-header">
-			<h5 class="mb-0 text-center">
-				<a data-toggle="collapse" href="#collapsePersonales3" aria-expanded="false" aria-controls="collapsePersonales3">
-					Datos personales
-				</a>
-			</h5>
-		</div>
-		<div id="collapsePersonales3" class="collapse show boxcollapse">
-			<div class="box-body">
-				@include('fields.personales-abo')
-			</div>
-		</div>
-	</div>
+	
 
-	<div class="card">
-		<div class="card-header">
-			<h5 class="mb-0 text-center">
-				<a data-toggle="collapse" href="#collapseTrab3" aria-expanded="false" aria-controls="collapseTrab3">
-					Datos del trabajo
-				</a>
-			</h5>
-		</div>
-		<div id="collapseTrab3" class="collapse boxcollapse">
-			<div class="box-body">
-				@include('fields.lugartrabajo')
-			</div>
-		</div>
-	</div>
 
-	<div class="card">
-		<div class="card-header">
-			<h5 class="mb-0 text-center">
-				<a data-toggle="collapse" href="#collapseAutoridad" aria-expanded="false" aria-controls="collapseAutoridad">
-					Información sobre el abogado
-				</a>
-			</h5>
-		</div>
-		<div id="collapseAutoridad" class="collapse boxcollapse">
-			<div class="box-body">
-				@include('fields.extra-abo')
-			</div>
+<div id="abogadotabs">
+	<ul class="nav nav-tabs">
+ 		<li class="nav-item">
+    		<a class="nav-link active" data-toggle="tab" href="#collapsePersonales3">Datos Personales</a>
+  		</li>
+  		<li class="nav-item">
+    		<a class="nav-link" data-toggle="tab" href="#collapseTrab3">Datos del trabajo</a>
+  		</li>
+  		<li class="nav-item" id="tabfisica3">
+    		<a class="nav-link" data-toggle="tab" href="#collapseAutoridad">Información sobre el abogado</a>
+  		</li>
+  		
+	</ul>
+</div>
+
+<div class="tab-content" id="contenidotabs">
+	<div class="tab-pane active container" id="collapsePersonales3">
+  		<div class="box-body">
+		@include('fields.personales-abo')
 		</div>
 	</div>
+<div class="tab-pane container" id="collapseTrab3">
+  		<div class="box-body">
+		@include('fields.lugartrabajo')
+		</div>  
+</div>
+<div class="tab-pane container" id="collapseAutoridad">
+	<div class="box-body">
+	@include('fields.extra-abo')
+	</div>
+</div>
+
+</div>
+
+
+
+	
+
+	
+
 	{!! Form::close() !!}
 	<div class="box-body">
 		@include('tables.abogados')
